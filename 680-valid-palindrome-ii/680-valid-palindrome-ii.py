d7ad4d678e39f3748ec93e16a4ca66a45dd64d5a
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-02 22:45:14 +0530

    Time: 99 ms (93.08%), Space: 14.6 MB (19.85%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,8 +1,50 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        h, t = 0, len(s) - 1  # head and tail
-        while h < t:
-            if s[h] != s[t]:  # delete s[h] or s[t] and validate palindrome finally
-                 return s[h:t] == s[h:t][::-1] or s[h + 1:t + 1] == s[h + 1:t + 1][::-1]
-            h, t = h + 1, t - 1
-        return True
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+#         # Approach 1: Backtrack
+#         # O(n^2) time O(n) space
+        
+#         def isPalindrome(word):
+#             l, r = 0, len(word)-1
+#             while l <= r:
+#                 if word[l] != word[r]:
+#                     return False
+#                 l+=1
+#                 r-=1
+                
+#             return True
+        
+#         def backtrack(idx, currWord):
+#             if idx == len(s)+1:
+#                 return False
+#             if isPalindrome(currWord):
+#                 return True
+            
+#             return backtrack(idx+1, s[:idx] + s[idx+1:])
+        
+#         return backtrack(0, s)
+    
+#         # Approach 2: Iteration
+#         # O(n^2) time O(1) space
+        
+#         for i in range(len(s)):
+#             if isPalindrome(s[:i]+s[i+1:]):
+#                 return True
+            
+#         return False
+    
+        # Approach 3:
+        
+        if s == s[::-1]:
+            return True
+        l, r = 0, len(s)-1
+        while l <= r:
+            if s[l] == s[r]:
+                l+=1
+                r-=1
+            else:
+                removeL = s[:l] + s[l+1:]
+                removeR = s[:r] + s[r+1:]
+                return removeL == removeL[::-1] or removeR == removeR[::-1]
+                
+       
