7e11b24027c04f16629ea71d8fb44903e3859a01
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-04 21:47:19 +0530

    Time: 1134 ms (81.17%), Space: 48.4 MB (53.33%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -5,16 +5,37 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        curr = head
-        arr = []
-        while curr:
-            arr.append(curr.val)
-            curr = curr.next
-        arr[k-1] , arr[-k] = arr[-k] , arr[k-1]
-        new_lst = curr = ListNode(0)
-        for i in arr:
-            curr.next = ListNode(i)
-            curr = curr.next
-        return new_lst.next
+        dummy = ListNode(0)
+        dummy.next = head
         
+        # start and end pointer:
+        fast, slow = dummy, dummy
+         
+        for i in range(k):
+            prev_fast = fast # the node right before the fast (k-1)th node
+            fast = fast.next # fast is at the kth node after iteration
             
+        kth_node = fast
+        
+        while fast.next:
+            slow = slow.next # slow is at the (k+1)th node from the last
+            fast = fast.next
+            
+        end_kth_node = slow.next
+        temp = kth_node.val
+        kth_node.val = end_kth_node.val
+        end_kth_node.val = temp
+        
+#         end_kth_node = slow.next # the kth node from the last
+#         slow.next = kth_node
+#         prev_fast.next = end_kth_node
+        
+
+#         kth_next = kth_node.next                
+#         kth_node.next = end_kth_node.next
+#         end_kth_node.next = kth_next
+
+        return dummy.next
+        
+        
+        
