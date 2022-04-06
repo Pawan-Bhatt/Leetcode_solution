dab6120bd1519691b97f37299e2d53b305c4ce47
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-06 13:17:12 +0530

    Time: 138 ms (56.39%), Space: 14 MB (92.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -1,24 +1,29 @@
 class Solution:
-    max_val = 10 ** 9 + 7
-    def threeSumMulti(self, arr: List[int], target: int) -> int:
-        res = 0
-        n = len(arr)
-        
-        # i: obj, j: weight, k: choice -> cnt
-        dp = [[[0] * 3 for _ in range(target + 1)] for _ in range(n)]
-        dp[0][arr[0]][0] = 1
-        
-        for i in range(1, n):
-            for j in range(target + 1):
-                dp[i][j][0] = dp[i-1][j][0]
-                if j == arr[i]:
-                    dp[i][j][0] += 1
+    def threeSumMulti(self, A: List[int], target: int) -> int:
+        MOD = 1_000_000_007
+        seen = Counter(A)
+        A = list(set(A))
+        A.sort()
+        n = len(A)
+        ans = 0
+        for i in range(n):
+            need = target - A[i]
+            obs = set()
+            for x in A[i:]:
+                obs.add(need - x)
+                if x > need:
+                    break
+                if x in obs:
+                    a, b, c = sorted([A[i], x, need - x])
+                    if a < b < c:
+                        ans += seen[a] * seen[b] * seen[c]
+                    if a == b < c:
+                        ans += seen[a] * (seen[a] - 1) * seen[c] // 2
+                    if a < b == c:
+                        ans += seen[a] * seen[b] * (seen[b] - 1) // 2
+                    if a == b == c:
+                        ans += seen[a] * (seen[a] - 1) * (seen[a] - 2) // 6
+                    ans %= MOD
                     
-                for k in range(1, 3):
-                    if j - arr[i] >= 0:
-                        dp[i][j][k] = (dp[i-1][j][k] + dp[i-1][j-arr[i]][k-1]) % self.max_val
-                    else:
-                        dp[i][j][k] = dp[i-1][j][k]
-
-        return dp[n-1][target][2]
+        return ans
         
