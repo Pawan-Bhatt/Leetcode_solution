75281a7df66d97dadd9ab7ce25644ad930e8fc33
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-10 10:57:24 +0530

    Time: 40 ms (93.04%), Space: 14.1 MB (34.49%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        ary = []
+        for i in range(len(ops)):
+            if ops[i]=="C":
+                ary.pop()
+            elif ops[i]=="D":
+                ary.append(ary[len(ary)-1]*2)
+            elif ops[i]=="+":
+                ary.append(ary[len(ary)-1]+ary[len(ary)-2])
+            else:
+                ary.append(int(ops[i]))
+        return sum(ary)
+    
