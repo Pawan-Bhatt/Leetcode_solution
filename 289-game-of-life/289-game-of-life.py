0b184c6e92031fe99c416d5a05b50591c75e73e1
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-12 21:36:21 +0530

    Time: 40 ms (71.83%), Space: 14 MB (51.25%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -1,28 +1,34 @@
 class Solution:
-    def gameOfLife(self, b: List[List[int]]) -> None:
+    def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
                 # all new 0's denotes as -1, (1 ==> 0)
-        # all new 1's denotes as 2   (0 ==> 1)
-        m, n = len(b), len(b[0])
-        dirs = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
-        for i in range(m):
-            for j in range(n):
-                livecount = 0
-                for r, c in dirs:
-                    nr, nc = i + r, j + c
-                    if 0 <= nr < m and 0 <= nc < n and abs(b[nr][nc]) == 1: # originally 1's
-                        livecount += 1
-                if b[i][j] == 1:
-                    if livecount < 2 or livecount > 3:   
-                        b[i][j] = -1
-                else:
-                    if livecount == 3:  
-                        b[i][j] = 2
-        
-        for i in range(m):
-            for j in range(n):
-                if b[i][j] == 2:    b[i][j] = 1
-                elif b[i][j] == -1: b[i][j] = 0
-        
+        ROWS, COLS = len(board), len(board[0])
+        def countneg(r,c):
+            neig = 0
+            #starting from left top corner
+            for i in range(r - 1, r + 2):
+                for j in range(c - 1, c+ 2):
+                    #position out of bounds
+                    if ((i == r and j == c) or i < 0 or j < 0  or i == ROWS or j == COLS):
+                        continue
+                    if board[i][j] in [1,3]:
+                        #count the original neighb
+                        neig += 1
+            return neig
+        for r in range(ROWS):
+               for c in range(COLS):
+                    neig = countneg(r,c)
+                    if board[r][c]:
+                        if neig in [2,3]:
+                            board[r][c] = 3
+
+                    elif neig == 3:
+                            board[r][c] = 2
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 1:
+                    board[r][c] = 0
+                elif board[r][c] in [2,3]:
+                    board[r][c] = 1
