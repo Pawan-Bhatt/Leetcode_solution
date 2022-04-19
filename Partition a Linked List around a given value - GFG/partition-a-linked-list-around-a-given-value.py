10caffcfbfff6356571b11595589d86778eb6fcd
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-04-19 20:35:21 +0530

    Added solution - LeetHub

--- Partition a Linked List around a given value - GFG/partition-a-linked-list-around-a-given-value.py
+++ Partition a Linked List around a given value - GFG/partition-a-linked-list-around-a-given-value.py
@@ -9,37 +9,24 @@ class Node:
 
 """
 class Solution:
-    def insert_to_list(self, node, list_tail):
-        node.next = None
-        list_tail.next = node
-        return node
-        
     def partition(self, head, x):
-        #code here
-        less_head = less_itr = Node(None)
-        equal_head = equal_itr = Node(None)
-        greater_head = greater_itr = Node(None)
-        itr = head
-        while itr:
-            future = itr.next
-            if itr.data < x:
-                less_itr = self.insert_to_list(itr, less_itr)
-            elif itr.data == x:
-                equal_itr = self.insert_to_list(itr, equal_itr)
+        j=[];k=[];n=head;t=[]
+        while head is not None:
+            if head.data<x:
+                j.append(head)
+            elif head.data==x:
+                t.append(head)
             else:
-                greater_itr = self.insert_to_list(itr, greater_itr)
-            itr = future
-            
-        less_head = less_head.next
-        equal_head = equal_head.next
-        greater_head = greater_head.next
-        
-        if equal_head:
-            equal_itr.next = greater_head
-        if less_head:
-            less_itr.next = equal_head or greater_head
-        head = less_head or equal_head or greater_head
-        return head
+                k.append(head)
+            head=head.next
+        p=j+t+k;m=1
+        for i in p:
+            if i==p[-1]:
+                i.next=None
+                break
+            i.next=p[m]
+            m=m+1
+        return p[0]
 
         #code here
 
