8e42708326681027a41c3de2f2b4ff9cc8051298
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-06-14 21:31:58 +0530

    Time: 135 ms (98.85%), Space: 13.9 MB (96.71%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,11 +1,14 @@
+import bisect
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m,n=len(word1),len(word2)
-        @cache
-        def lcs(i, j): # find longest common subsequence
-            if i==m or j==n:
-                return 0            
-            return 1 + lcs(i+1, j+1) if word1[i]==word2[j] else  max(lcs(i+1, j), lcs(i,j+1))                               
-        # subtract the lcs length from both the strings 
-        # the difference is the number of characters that has to deleted
-        return m + n - 2*lcs(0,0)
+        dp = []
+        for s in word2:
+            for i in reversed(range(len(word1))):
+                v = word1[i]
+                if v == s:
+                    ind = bisect.bisect_left(dp, i)
+                    if ind == len(dp):
+                        dp.append(i)
+                    else:
+                        dp[ind] = i
+        return len(word1) + len(word2) - len(dp) * 2   
