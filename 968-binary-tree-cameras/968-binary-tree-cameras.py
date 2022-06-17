39eb2ce7fd5bc755a94691e81558266d970c816f
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-06-17 17:48:07 +0530

    Time: 75 ms (36.23%), Space: 14.3 MB (21.59%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -1,17 +1,32 @@
-class Solution(object):
-    def minCameraCover(self, root):
-        self.ans = 0
-        covered = {None}
-
-        def dfs(node, par = None):
-            if node:
-                dfs(node.left, node)
-                dfs(node.right, node)
-
-                if (par is None and node not in covered or
-                        node.left not in covered or node.right not in covered):
-                    self.ans += 1
-                    covered.update({node, par, node.left, node.right})
-
-        dfs(root)
-        return self.ans
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        MONITOR_HASCAMERA=0
+        MONITOR_NOCAMERA=1
+        NOT_MONITORED=2
+        def checkCam(root,par)->int:
+            nonlocal ans
+            if root==None:
+                return MONITOR_NOCAMERA
+            left=checkCam(root.left,root)
+            right=checkCam(root.right,root)
+            if left==MONITOR_NOCAMERA and right==MONITOR_NOCAMERA:
+                return NOT_MONITORED
+            elif left==NOT_MONITORED or right==NOT_MONITORED:
+                ans+=1
+                return MONITOR_HASCAMERA
+            else:
+                return MONITOR_NOCAMERA
+        ans=0
+        val=checkCam(root,None)
+        if val==NOT_MONITORED:
+            ans+=1
+        return ans
+        
+            
+        
