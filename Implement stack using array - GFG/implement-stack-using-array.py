3ef1bc72cf7bfa2dc77985fc9f6609f94eee73f0
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2023-01-18 12:10:41 +0530

    Added solution - LeetHub

---
+++ Implement stack using array - GFG/implement-stack-using-array.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        self.arr.append(data)
+    def pop(self):
+        if len(self.arr) ==0:
+            return -1
+        return self.arr.pop()
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyStack()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+# } Driver Code Ends
