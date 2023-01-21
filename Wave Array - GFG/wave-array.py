470be5c2d69811ec5422a80605e674d7f069582c
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2023-01-21 08:58:34 +0530

    Added solution - LeetHub

---
+++ Wave Array - GFG/wave-array.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def convertToWave(self, n : int, a : List[int]) -> None:
+        i = 0
+        while i < n-1:
+            a[i],a[i+1]=a[i+1],a[i]
+            i+=2
+        # code here
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        a=IntArray().Input(n)
+        
+        obj = Solution()
+        obj.convertToWave(n, a)
+        IntArray().Print(a)
+
+# } Driver Code Ends
